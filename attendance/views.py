67989614deba2f438769d09f25614d78d7fd9070
aku672003 <161923345+aku672003@users.noreply.github.com>
2026-02-27 12:48:10 +0530

    session27/02

--- attendance/views.py
+++ attendance/views.py
@@ -3531,7 +3531,6 @@ def error_500_view(request):
     return render(request, '500.html', status=500)
 
 
-@require_valid_token
 def spa_view(request):
     """Protected view to serve the SPA index.html."""
     return render(request, 'index.html')
